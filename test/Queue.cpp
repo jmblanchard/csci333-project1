774da90450dff551b2da14ff2cbb34325d0f1376
Jason Blanchard <jasonmblanchard@gmail.com>
2012-09-21 10:47:13 -0400

    Added test for LQueue.

--- test/Queue.cpp
+++ test/Queue.cpp
@@ -50,3 +50,53 @@ TEST(AQueueTest, IsEmpty) {
     a->dequeue();
     EXPECT_EQ(true, a->isEmpty());
 }
+
+TEST(LQueueTest, Enqueue) {
+    LQueue *l = new LQueue();
+    for (int i = 0; i < 25; ++i) {
+        l->enqueue(i);
+        EXPECT_EQ(i+1, l->size());
+    }
+
+    delete l;
+}
+
+TEST(LQueueTest, Dequeue) {
+    LQueue *l = new LQueue();
+    int i = 0;
+
+    for (i = 0; i < 25; ++i) {
+        l->enqueue(i);
+    }
+
+    for (i = 0; i < 25; ++i) {
+        EXPECT_EQ(i, l->dequeue());
+        EXPECT_EQ(24-i, l->size());
+    }
+}
+
+TEST(LQueueTest, Size) {
+    LQueue *l = new LQueue();
+    EXPECT_EQ(0, l->size());
+
+    for (int i = 0; i < 10; ++i) {
+        l->enqueue(i);
+        EXPECT_EQ(i+1, l->size());
+    }
+
+    for (int i = 10; i > 0; --i) {
+        l->dequeue();
+        EXPECT_EQ(i-1, l->size());
+    }
+}
+
+TEST(LQueueTest, IsEmpty) {
+    LQueue *l = new LQueue();
+    EXPECT_EQ(true, l->isEmpty());
+
+    l->enqueue(1);
+    EXPECT_EQ(false, l->isEmpty());
+
+    l->dequeue();
+    EXPECT_EQ(true, l->isEmpty());
+}
