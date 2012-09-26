27230318b090f9e13a72a3fbd23ab312a2cf2d75
Jason Blanchard <jasonmblanchard@gmail.com>
2012-09-26 11:47:03 -0400

    Added some timing stuff, not working correctly at the moment.

--- src/queue_tester.cpp
+++ src/queue_tester.cpp
@@ -2,27 +2,72 @@
 #include "LQueue/LQueue.h"
 #include "LLQueue/LLQueue.h"
 #include <iostream>
+#include <time.h>
+
+#define NUM_ITER 20000
 
 int main() {
     AQueue aqueue(5);
     LQueue *lqueue = new LQueue();
     LLQueue *llqueue = new LLQueue();
 
-    for (int i = 0; i < 20; ++i) {
+    int start, end, elapsed;
+    double time;
+
+    start = clock();
+    for (int i = 0; i < NUM_ITER; ++i) {
         aqueue.enqueue(i);
+    }
+    end = clock();
+    elapsed = end - start;
+    time = (double)elapsed/CLOCKS_PER_SEC;
+    std::cout << "AQueue enqueue timing: " << time << std::endl;
+
+    start = clock();
+    for (int i = 0; i < NUM_ITER; ++i) {
         lqueue->enqueue(i);
+    }
+    end = clock();
+    elapsed = end - start;
+    time = (double)elapsed/CLOCKS_PER_SEC;
+    std::cout << "LQueue enqueue timing: " << time << std::endl;
+
+
+    start = clock();
+    for (int i = 0; i < NUM_ITER; ++i) {
         llqueue->enqueue(i);
     }
+    end = clock();
+    elapsed = end - start;
+    time = (double)elapsed/CLOCKS_PER_SEC;
+    std::cout << "LLQueue enqueue timing: " << time << std::endl;
 
-    std::cout << "LLQueue size " << llqueue->size() << std::endl;
-
-    for (int i = 0; i < 20; ++i) {
-        std::cout << aqueue.dequeue() << std::endl;
-        std::cout << lqueue->dequeue() << std::endl;
-        std::cout << llqueue->dequeue() << std::endl;
+    start = clock();
+    for (int i = 0; i < NUM_ITER; ++i) {
+        aqueue.dequeue();
     }
+    end = clock();
+    elapsed = end - start;
+    time = (double)elapsed/CLOCKS_PER_SEC;
+    std::cout << "AQueue dequeue timing: " << time << std::endl;
 
-    std::cout << "LLQueue size " << llqueue->size() << std::endl;
+    start = clock();
+    for (int i = 0; i < NUM_ITER; ++i) {
+        lqueue->dequeue();
+    }
+    end = clock();
+    elapsed = end - start;
+    time = (double)elapsed/CLOCKS_PER_SEC;
+    std::cout << "LQueue dequeue timing: " << time << std::endl;
+
+    start = clock();
+    for (int i = 0; i < NUM_ITER; ++i) {
+        llqueue->dequeue();
+    }
+    end = clock();
+    elapsed = end - start;
+    time = (double)elapsed/CLOCKS_PER_SEC;
+    std::cout << "LLQueue dequeue timing: " << time << std::endl;
 
     delete lqueue;
     delete llqueue;
